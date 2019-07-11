d3cece25e2e0aac602d2a2606631b8e2d05c760e
auhide <bo0opr@gmail.com>
2019-07-11 23:17:14 +0300

    Added the Many-To-Many articles-keywords insertion functionality

--- form/form_php.php
+++ form/form_php.php
@@ -2,7 +2,8 @@
     include_once 'D:\tu-cce\includes\dbh.inc.php';
     include_once 'insertion_funcs.php';
 
-    // Taking the variables from the Form
+
+    // Taking the variables from the Form in index.php
     $first = $_POST["first"];
     $last = $_POST["last"];
     $keywords =  explode(",", trim(preg_replace('/\s+/', '', $_POST['keywords'])));
@@ -12,12 +13,18 @@
     $edition = explode("/", trim(preg_replace('/\s+/', '', $_POST['edition'])));
 
 
+    // If you don't insert and article you cant insert keywords
     // Inserting to Articles table
     if($title and $abstract and $url and $conn){ 
         articles_insert($title, $abstract, $url, $conn);
+
+        // Inserting into Keywords table 
+        if(!empty($keywords) and $keywords[0] != ""){
+            keywords_insert($keywords, $conn);
+            
+            // Inserting into the connecting Many-To-Many table
+            articles_keywords_insert(sizeof($keywords), $conn);
+        }
     }
 
-    // Inserting into Keywords table 
-    if(!empty($keywords) and $keywords[0] != ""){
-        keywords_insert($keywords, $conn); 
-    }
+
--- form/insertion_funcs.php
+++ form/insertion_funcs.php
@@ -27,7 +27,7 @@
 
         foreach($keywords as $word ){
             $word_search = "SELECT word FROM keywords
-                            WHERE word = '$word';";
+                             WHERE word = '$word';";
     
             $query_matches = mysqli_query($connection, $word_search);
     
@@ -36,10 +36,43 @@
             if($row_count == 0){
             
                 $sql_keyword = "INSERT INTO keywords 
-                                    (word)
-                           VALUES   ('$word');";
+                                        (word)
+                                VALUES  ('$word');";
     
                 $connection -> query($sql_keyword);
             }
         }
+    }
+
+    function get_last_id($table_name, $connection){
+        $last_article_query = "SELECT id FROM". " $table_name ".
+                               "ORDER BY id DESC
+                               LIMIT 1;";
+
+        $query_matches = mysqli_query($connection, $last_article_query);
+
+        $last_id = mysqli_fetch_assoc($query_matches)["id"];
+
+        return $last_id;
+    }
+
+
+    function articles_keywords_insert($keywords_len, $connection){
+        
+        // Getting the id of the last article
+        $last_article_id = get_last_id('articles', $connection);
+
+        // Getting the id of the last keyword
+        $last_keyword_id = get_last_id('keywords', $connection);
+
+        while($keywords_len > 0){
+            $query = "INSERT INTO article_keywords
+                             (article_id, keyword_id)
+                      VALUES ('$last_article_id', '$last_keyword_id');";
+            $connection -> query($query);
+
+            $last_keyword_id--;
+            $keywords_len--;
+        }
+
     }
