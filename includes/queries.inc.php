2c83315272635c89269b016125aaa7ab276c905e
auhide <bo0opr@gmail.com>
2019-07-15 19:35:50 +0300

    A version of a the searching function

--- includes/queries.inc.php
+++ includes/queries.inc.php
@@ -2,13 +2,80 @@
 
 <?php
 
-function queryByName($first, $last){
-    $sql_names =    "SELECT authors.f_name, authors.l_name, articles.title, articles.abstract FROM article_authors
-                    JOIN articles ON articles.id = article_authors.article_id
-                    JOIN authors ON authors.id = article_authors.author_id
-                    WHERE authors.f_name = '$first' AND authors.l_name = '$last';";
+$SQL_QUERY =    "SELECT distinct art.title, art.abstract , 
+                    (SELECT group_concat(keywords.word) 
+                    FROM keywords 
+                    JOIN article_keywords ON keywords.id = article_keywords.keyword_id
+                    WHERE article_keywords.article_id = art_aut.article_id 
+                    GROUP BY article_keywords.article_id
+                    ) AS keywords, 
+                                
+                    (SELECT group_concat(authors.f_name,' ',authors.l_name)
+                    FROM authors 
+                    JOIN article_authors ON authors.id = article_authors.author_id
+                    WHERE article_authors.article_id = art_aut.article_id
+                    GROUP BY article_authors.article_id
+                    ) AS authors, 
+                                
+                    e.year AS 'EditionYear',
+                    e.number AS 'EditionNumber'
+                    FROM article_authors art_aut
+                    JOIN articles art ON art.id = art_aut.article_id
+                    JOIN authors aut  ON aut.id = art_aut.author_id
+                    JOIN article_keywords art_key ON art.id = art_key.article_id
+                    JOIN keywords k on art_key.keyword_id = k.id
+                    JOIN editions e on art.id = e.article_id" . "\n WHERE ";
 
-    return $sql_names;
+
+function get_where_queries($inputs, $first, $last, $keywords, $title, $edition){
+    /**
+      * Returns an associative array with all parts of the WHERE statement that is used to search.
+      *
+      * @param array    $inputs
+      * @param string   $first
+      * @param string   $last
+      * @param array    $keywords
+      * @param string   $title
+      * @param array    $edition
+    */
+
+    foreach($inputs as $key => $input){
+        switch($key){
+            case "first":
+                if($first != ''){
+                    $inputs[$key] = "LOWER(aut.f_name) = '$first'";
+                }
+                break;
+            case "last":
+                if($last != ''){
+                    $inputs[$key] = "LOWER(aut.l_name) = '$last'";
+                }
+                break;
+            case "keywords":
+                if($keywords[0] != ''){
+                    $kws = join(",", $keywords);
+                    $inputs[$key] = "LOWER(k.word) like '$kws'";
+                }
+                break;
+            case "title":
+                if($title != ''){
+                    $inputs[$key] = "LOWER(art.title) like ('$title')";
+                }
+                break;
+                
+            case "year":
+                if($edition[0] != ''){
+                    $inputs[$key] = " e.year = '$edition[0]' ";
+                }
+                break;
+            case "number":
+                if(isset($edidtion[1])){
+                    if($edition[1] != ''){
+                        $inputs[$key] = " e.number = '$edition[1]' ";
+                    }
+                }
+        }
+    }
+
+    return $inputs;
 }
-
-?>
--- results.php
+++ results.php
@@ -3,31 +3,46 @@
     // Including the DB connection
     include_once 'includes/dbh.inc.php';
     // Including the SQL Queries
-    // include_once 'includes/queries.inc.php';
+    include_once 'includes/queries.inc.php';
+
+    $inputs_entered['first'] = "";
+    $inputs_entered['last'] = "";
+    $inputs_entered['keywords'] = "";
+    $inputs_entered['title'] = "";
+    $inputs_entered['year'] = "";
+    $inputs_entered['number'] = "";
+
 
     // Taking the variables from the Form
-    $first = strtolower(preg_replace('/\s+/', '', $_POST['first']));
-    $last = strtolower(preg_replace('/\s+/', '', $_POST['last']));
+    $first =    mysqli_escape_string($conn, strtolower(preg_replace('/\s+/', '', $_POST['first'])));
+    $last =     mysqli_escape_string($conn, strtolower(preg_replace('/\s+/', '', $_POST['last'])));
     $keywords = explode(",", trim(preg_replace('/\s+/', '', $_POST['keywords']))); // Splitting the keywords
-    $title = strtolower(trim($_POST['title']));
-    $edition = explode("/", trim(preg_replace('/\s+/', '', $_POST['edition'])));
-    
+    $title =    mysqli_escape_string($conn, strtolower(trim($_POST['title'])));
+    $edition =  explode("/", trim(preg_replace('/\s+/', '', $_POST['edition'])));
 
-    $sql_names =    "SELECT authors.f_name, authors.l_name, articles.title, articles.abstract FROM article_authors
-                     JOIN articles ON articles.id = article_authors.article_id
-                     JOIN authors ON authors.id = article_authors.author_id
-                     WHERE LOWER(authors.f_name) = '$first' OR LOWER(authors.l_name) = '$last';";
+    $inputs_entered = get_where_queries($inputs_entered, $first, $last, $keywords, $title, $edition);
+
+    $inputs_entered = array_filter($inputs_entered);
+
+    // $SQL_QUERY is imported from 'includes/queries.inc.php'
+    $search_query = $SQL_QUERY . join(" AND \n" ,$inputs_entered) . ";";
     
+    // echo $search_query;
+
     // Sending the Query to the DB
-    $results = mysqli_query($conn, $sql_names); // $conn is imported from the /includes/ folder
-    
+    $results = mysqli_query($conn, $search_query); // $conn is imported from the /includes/ folder
+
+
     // Getting the count of the rows, given by the result of the Query
     $result_check = mysqli_num_rows($results);
 
     if($result_check > 0){
         while($row = mysqli_fetch_assoc($results)){
             // Here $row becomes an associative array
-            echo '<strong>Author</strong>: ' . $row['f_name'] . " " . $row['l_name'] . "<br>" . '<strong>Title</strong>: ' . $row['title'] . "<br>" . '<strong>Abstract</strong>: ' . $row['abstract'] ."<br><br>";
+            echo '<br><strong>Authors</strong>: ' . $row['authors'] . "<br>" . 
+                 '<strong>Title</strong>: ' . $row['title'] . "<br>" . 
+                 '<strong>Abstract</strong>: ' . $row['abstract'] . "<br>" .
+                 '<strong>Keywords</strong>: ' . $row['keywords'] ."<br><br>";;
         }
     }
     else{ echo 'No articles were found.'; }
