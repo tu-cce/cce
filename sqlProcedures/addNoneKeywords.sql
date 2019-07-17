03b64da8bfad240b4d149a6a415f5a3f3696867d
Bojidar Zahov <bzahov1998@gmail.com>
2019-07-17 17:04:37 +0300

    Create addNoneKeywords.sql

---
+++ sqlProcedures/addNoneKeywords.sql
@@ -0,0 +1,32 @@
+drop procedure if exists addNoneKeywords;
+delimiter |
+create procedure addNoneKeywords(IN noneKeywordID int)
+BEGIN
+    declare finished int;
+    declare tempArticleID int;
+    DECLARE myCursor CURSOR FOR(
+        Select articles.id
+        from articles
+        where articles.id not in (SELECT article_id
+                                  from article_keywords)
+    );
+    declare continue handler FOR NOT FOUND set finished = 1;
+    set finished = 0;
+
+    Open myCursor;
+    article_loop: while (finished = 0)
+    do
+        fetch myCursor into tempArticleID;
+        IF(finished = 1)
+        THEN
+            LEAVE article_loop;
+        END IF;
+        INSERT into article_keywords(article_id, keyword_id) VALUE(tempArticleID, noneKeywordID);
+    end while ;
+    close myCursor;
+    set finished = 0;
+end;
+|
+delimiter |
+
+call addNoneKeywords(8);
